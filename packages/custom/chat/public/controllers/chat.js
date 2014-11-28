961330d2f26af8b3b8e34c2e233bf49a57fd3cec
juanjmerono <jjmerono@um.es>
2014-11-28 22:49:26 +0100

    Chat ready

--- packages/custom/chat/public/controllers/chat.js
+++ packages/custom/chat/public/controllers/chat.js
@@ -6,17 +6,38 @@ angular.module('mean.chat').controller('ChatController', ['$scope', 'Global', 'C
     $scope.package = {
       name: 'chat'
     };
+    var wsUri = 'ws://'+window.location.host.replace('3000','3001');
+    var ws = new WebSocket(wsUri);
+    if (ws!==null) {
+		ws.onopen = function() {
+			ws.send('open::'+$scope.global.user.name);
+		};
+		ws.onmessage = function(message) {
+			var d = message.data.split('::');
+			if (d[0]==='open') {
+				$scope.alertMessage = d[1]+' se ha incorporado al chat !!';
+				$scope.$apply();
+			} else if (d[0]==='close') {
+				$scope.alertMessage = d[1]+' ha abandonado al chat !!';
+				$scope.$apply();
+			} else {
+				$scope.find();
+			}
+		};
+    }
     
     $scope.find = function() {
         Chat.query(function(chat) {
-          $scope.chat = chat;
+        	$scope.alertMessage = '';
+        	$scope.chat = chat;
         });
       };
     
     $scope.sendMessage = function () {
     	var chat = new Chat({content:this.content});
     	chat.$save(function(response) {
-          $scope.find();
+    		$scope.find();
+    		ws.send('new::-');
         });
     };
       
--- server.js
+++ server.js
@@ -2,9 +2,59 @@
 
 // Requires meanio
 var mean = require('meanio');
+var online = {
+	data:[],
+	connected:0,
+	add:function(obj) {
+		obj.id = this.connected;
+		this.connected += 1;
+		this.data.push({id:obj.id,value:obj});
+	},
+	remove:function(id) {
+		for (var x=0; x<this.data.length; x+=1) {
+			if(this.data[x].id===id) {
+				var usr = this.data[x].value.user;
+				this.data.splice(x,1);
+				return usr;
+			}
+		}
+		return null;
+	},
+	foreach:function(callback) {
+		for (var x=0; x<this.data.length; x+=1) {
+			callback(this.data[x].value);
+		}
+	}
+};
 
 // Creates and serves mean application
 mean.serve({ /*options placeholder*/ }, function(app, config) {
 	var port = config.https && config.https.port ? config.https.port : config.http.port;
 	console.log('Mean app started on port ' + port + ' (' + process.env.NODE_ENV + ')');
+	var WebSocketServer = require('ws').Server;
+	//var wss = new WebSocketServer({server: app});
+	var wss = new WebSocketServer({port: (port-0)+1});
+	wss.on('connection', function(ws) {
+		online.add(ws);
+		ws.on('message', function(message) {
+			var data = message.split('::');
+			online.foreach(function(current){
+				if (current.id !== ws.id) {
+					current.send(message);
+				} else {
+					if (data[0]==='open') {
+						current.user = data[1];
+					}
+				}
+			});
+		});
+		ws.on('close', function(){
+			var usr = online.remove(ws.id);
+			online.foreach(function(current){
+				current.send('close::'+usr);
+			});
+		});
+		ws.send('connected');
+	});
 });
+
